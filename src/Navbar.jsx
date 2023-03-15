0734547a72d865963ded243c28048e2da6e2ed0e
Musyoki <vincenbryaniel@gmail.com>
2023-03-15 13:44:28 +0300

    navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,3 +1,12 @@
+import react from 'react'
 function Navbar( ){
-    
+    return(
+        <div>
+            <nav>
+                
+            </nav>
+        </div>
+    )
+
 }
+export default Navbar
